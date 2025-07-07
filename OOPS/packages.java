9e8d13dbc08c11cca84858d820dcf1fd3831e424
Ashwani Singh <itsashksingh10@gmail.com>
2025-07-07 19:22:23 +0530

    added package,-cp, -d  operations

---
+++ OOPS/packages.java
@@ -0,0 +1,9 @@
+package OOPS;
+import static in.ashwanisingh.math.MathUtil.square;
+
+class packages {
+    public static void main(String[] args) {
+        square(2);
+    }
+    
+}
---
+++ in/ashwanisingh/math/MathUtil.java
@@ -0,0 +1,11 @@
+package in.ashwanisingh.math;
+public class MathUtil {
+    public static void square(int x) {
+        System.out.println(x*x);
+        
+    }
+    public static void cube(int x) {
+        System.out.println(x*x*x);
+        
+    }
+}
